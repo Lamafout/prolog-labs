7c464050058b06d528e83f8cfbb8f6cdb6f490c9
lamafout <dzordzvashington@gmail.com>
2025-05-24 00:32:18 +0300

    fix: L9T2

--- lab2/sum_nums.pl
+++ lab2/sum_nums.pl
@@ -1,9 +1,9 @@
 sum_numbers(X, Answer) :-
-    sum_numbers_tailed(X, 0, Answer).
+    sum_numbers(X, 0, Answer).
 
-sum_numbers_tailed(0, Acc, Acc) :- !.
+sum_numbers(0, Acc, Acc) :- !.
 
-sum_numbers_tailed(X, Acc, Answer) :-
+sum_numbers(X, Acc, Answer) :-
     X1 is X // 10,
     Acc1 is Acc + X mod 10,
-    sum_numbers_tailed(X1, Acc1, Answer).
+    sum_numbers(X1, Acc1, Answer).
